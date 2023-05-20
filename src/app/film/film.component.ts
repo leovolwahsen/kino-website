6f0bfe2dad86bcf98846ecd466160eb0b5c10f38
Leo <volwahsenl@gmail.com>
2023-05-20 21:41:12 +0200

    logig und styling einzelner filme eingefuegt

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-film',
   templateUrl: './film.component.html',
   styleUrls: ['./film.component.scss']
 })
-export class FilmComponent {
+export class FilmComponent implements OnInit {
 
+  type = "";
+  id = "";
+  url = "";
+  filme: any;
+  film: any;
+
+  constructor(private route: ActivationEnd, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.type = this.route.snapshot.params["type"];
+    this.id = this.route.snapshot.params["id"];
+    if (this.type === "beliebt") {
+      this.url = "http://localhost:4200/assets/data/beliebtefilme.json";
+    }
+    if (this.type === "kino") {
+      this.url = "http://localhost:4200/assets/data/kinofilme.json";
+    }
+    if (this.type === "trendig") {
+      this.url = "http://localhost:4200/assets/data/trendigefilme.json";
+    }
+    this.getFilm();
+  }
+
+  getFilm() {
+    this.http.get(this.url).subscribe((filme) => {
+      this.filme = filme;
+      let index = this.filme.findIndex(
+        (film: { id: string }) => film.id == this.id
+        );
+        if(index > -1) {
+          this.film = this.filme[index];
+        }
+    });
+  }
 }
