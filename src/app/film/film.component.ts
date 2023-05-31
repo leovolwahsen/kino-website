41f52cb7bf3207956c5d615adb8799b62286de21
Leo <volwahsenl@gmail.com>
2023-05-31 18:10:07 +0200

    versuche noch die videos von json zu holen

--- src/app/film/film.component.ts
+++ src/app/film/film.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { ElementRef } from '@angular/core';
 @Component({
   selector: 'app-film',
   templateUrl: './film.component.html',
@@ -15,9 +15,11 @@ export class FilmComponent implements OnInit {
   filme: any;
   film: any;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) { }
+
+  constructor(private route: ActivatedRoute, private http: HttpClient, private hostElement: ElementRef) { }
 
   ngOnInit(): void {
+    
     this.type = this.route.snapshot.params["type"];
     this.id = this.route.snapshot.params["id"];
     if (this.type === "beliebt") {
@@ -30,6 +32,8 @@ export class FilmComponent implements OnInit {
       this.url = "http://localhost:4200/assets/daten/trendinge-filme.json";
     }
     this.getFilm();
+    const iframe = this.hostElement.nativeElement.querySelector('iframe');
+    iframe.src = 'https://www.youtube.com/embed/b_yvKh_ps18';
   }
 
   getFilm() {
@@ -43,4 +47,5 @@ export class FilmComponent implements OnInit {
         }
     });
   }
+  
 }
